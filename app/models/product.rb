1da2aa14be5334a577115544e829410bce8f9aee
TIM JORIA <jtjoria@gmail.com>
2016-01-20 21:17:26 -0600

    using migrations to add columns

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ActiveRecord::Base
+
+  def sale_message
+    if price.to_f < 2
+      "Discount Item!!"
+    else 
+      "On Sale!!"
+    end
+  end
+
+  def sales_tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + sales_tax
+  end
+
 end
---
+++ db/migrate/20160121031415_change_sales_tax_of_products.rb
@@ -0,0 +1,7 @@
+class ChangeSalesTaxOfProducts < ActiveRecord::Migration
+  def change
+    change_table :products do |t|
+      t.change :sales_tax, :decimal, precision: 8, scale: 2
+    end
+  end
+end
